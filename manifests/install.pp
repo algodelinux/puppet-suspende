72b133199a694d969b284d1bb96a0a997d5a699e
Esteban M. Navas Martín <algodelinux@gmail.com>
2014-05-29 12:10:04 +0200

    Modificando dependencias en install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,16 +2,12 @@ class puppet-suspende::install {
 
   package { "pm-utils" :
     ensure => present,
-    require => Class["puppet-suspende::config"],
-    notify =>  Class["puppet-suspende::service"],
   }
 
   cron { suspende:
     command => "/usr/local/sbin/suspende.sh",
     user    => root,
     minute  => '*/15',
-    require => Class["puppet-suspende::config"],
-    notify =>  Class["puppet-suspende::service"],
   }
 }
 
